0eecb2215a7b0254d4c8bbdfc1d5145e3abaee3f
Raviraj2022 <ravirajsahu312@gmail.com>
2024-03-31 23:28:25 +0530

    I have made navbar and product list.

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import "./App.css";
+import Caursel from "./components/Caursel";
+import Card from "./components/Card";
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <Caursel />
+      <Card />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Card.js
@@ -0,0 +1,52 @@
+import React, { useEffect, useState } from "react";
+// import axios from "axios";
+const Card = () => {
+  const [cardData, setCardData] = useState([]);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("https://dummyjson.com/products");
+        // console.log(response);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        setCardData(data);
+        // console.log(data.products);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, []);
+  console.log(cardData.products);
+  return (
+    <div className=" container  mt-5">
+      <div className="row mt-5">
+        {cardData.products &&
+          cardData.products.map((card, index) => (
+            <div className="col-md-4" key={index}>
+              <div className="card">
+                <img
+                  src={card.thumbnail}
+                  className="card-img-top"
+                  alt={card.alt}
+                  style={{ height: "200px", width: "100%" }}
+                />
+                <div className="card-body">
+                  <h5 className="card-title">{card.title}</h5>
+                  <p className="card-text">{card.description}</p>
+                  <a href={card.link} className="btn btn-primary">
+                    view more...
+                  </a>
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+};
+
+export default Card;
